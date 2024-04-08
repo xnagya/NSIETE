ccdfec88b688a97b7804dde66e4f13b221c01558
Matúš Srnec <matulikiki@gmail.com>
2024-04-08 20:56:24 +0200

    add label visualization

--- zadanie2/net_config.py
+++ zadanie2/net_config.py
@@ -1,4 +1,5 @@
 from argparse import Namespace
+import torch
 
 def config_to_dict(ns: Namespace):
     return vars(ns)
@@ -20,7 +21,7 @@ config_Unet = Namespace (
     channels_out_init = 32,             # initial channel width (num of output channels on 1-st layer)
     channel_mul = float(2),             # multiplication of image channels per layer, idealy int number
     network_depth = 3,                  # number of network layers (without bridge)
-    skip_features = "concat",           # none | concat | sdi    
+    skip_features = "none",           # none | concat | sdi    
 
     # ENC, DEC params
     # h,w should be consistent per layer (otherwise output has different h,w then input)
@@ -31,5 +32,17 @@ config_Unet = Namespace (
     dilation = 1,                       # affects h,w 
     pool_type = "max",                  # max | avg 
     pool_kernel_size = 2,               # affects h,w (pooling and transposed convolution)
-    padding_convT = [1, 0, 0, 0]        # needed if h,w is not divisible by at i-layer (1 = not divisible)
+    padding_convT = [1, 0, 0]        # needed if h,w is not divisible by at i-layer (1 = not divisible)
+)
+
+
+class_colors = torch.IntTensor([
+    [0, 0, 0]           #black
+    , [0, 191, 255]     #deep sky blue
+    , [220, 20, 60]     #crimson
+    , [34, 139, 34]     #forest green
+    , [238, 130, 238]   #violet
+    , [255, 255, 0]     #yellow
+    , [192, 192, 192]   #silver
+    ]
 )
