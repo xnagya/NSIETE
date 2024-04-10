79658e24d410dfcf2ea93634c2dfff807d44ba92
Matúš Srnec <matulikiki@gmail.com>
2024-04-10 09:14:03 +0200

    reduce kernel

--- zadanie2/net_config.py
+++ zadanie2/net_config.py
@@ -33,22 +33,22 @@ save_interval = 3
 
 # U-Net params
 config_Unet = Namespace (
-    channels_out_init = 16,             # initial channel width (num of output channels on 1-st layer)
+    channels_out_init = 32,             # initial channel width (num of output channels on 1-st layer)
     channel_mul = float(2),             # multiplication of image channels per layer, idealy int number
     network_depth = 3,                  # number of network layers (without bridge)
     skip_features = "concat",           # none | concat     
 
     # ENC, DEC params
     # h,w should be consistent per layer (otherwise output has different h,w then input)
-    block_width = 3,            
-    kernel_size = 5,                    # affects h,w 
-    padding = 2,                        # affects h,w 
+    block_width = 2,            
+    kernel_size = 3,                    # affects h,w 
+    padding = 1,                        # affects h,w 
     stride = 1,                         # affects h,w 
     dilation = 1,                       # affects h,w 
     pool_type = "max",                  # max | avg 
     pool_kernel_size = 3,               # affects h,w (pooling and transposed convolution)
     padding_convT = [1, 1, 2],          # needed when h,w is not divisible by at i-layer (1 = not divisible)
-
+    # U-Net V2
     channel_att_ratio = 16,
     SDI_channels = 32
 )
